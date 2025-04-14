629ca55cb39cc220d157b503a470eba48a05e5c8
Shohinur <shohinur@ShohinucBookAir.lan>
2025-04-14 15:19:41 +0900

    Initial commit on develop branch

---
+++ src/app/MaterialTheme/MaterialTheme/train.ts
@@ -0,0 +1,8 @@
+function calculate(expression: string): number {
+    return expression
+      .split("+")
+      .map(num => parseInt(num))
+      .reduce((sum, num) => sum + num, 0);
+  }
+  
+  console.log(calculate("1+3"));
