4debe41b5ed91f2d479c97b212a376c4642b7a05
Shohinur <shohinur@Shohinurui-MacBookAir.local>
2025-04-17 10:36:43 +0900

    feat: organize heder and footer components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,53 +1,40 @@
 import React from "react";
 import "../css/app.css";
-import { Link, BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, useLocation } from "react-router-dom";
 import { HomePage} from "./screens/homePage";
 import { ProductsPage } from './screens/productsPage/index';
 import { OrdersPage } from './screens/ordersPage/index';
 import { UserPage } from './screens/userPage/index';
+import { HomeNavbar } from './components/HomeNavbar';
+import { OtherNavbar } from './components/OtherNavbar'
+import { Footer } from "./components/footer";
 
-export default function App() {
+ function App() {
+  const location =useLocation();
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">HomePage</Link>
-            </li>
-            <li>
-              <Link to="/products">ProductsPage</Link>
-            </li>
-            <li>
-              <Link to="/orders">OrdersPage</Link>
-            </li>
-            <li>
-              <Link to="/member-page">UserPage</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/products">
+    <>
+    {location.pathname === "/" ?  <HomeNavbar/> : <OtherNavbar/>}
+  <Router>
+      <Switch>
+        <Route path="/products">
           <ProductsPage />
-          </Route>
-          <Route path="/orders">
-            <OrdersPage />
-            <Route path="/member-page">
+        </Route>
+        <Route path="/orders">
+          <OrdersPage />
+        </Route>
+        <Route path="/member-page">
           <UserPage />
-          </Route>
-          </Route>
-          <Route path="/">
-            <HomePage />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+  </Router>
+  <Footer />
+  </>
+);
 }
-
+export default App;
 
 
 
